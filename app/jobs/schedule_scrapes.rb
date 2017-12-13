e3dad4235874aa189b178ccfee57b2fb04af7a4f
Jean Brito <jeanfbrito@gmail.com>
2017-12-13 20:30:27 -0200

    speedup scrape

--- app/jobs/schedule_scrapes.rb
+++ app/jobs/schedule_scrapes.rb
@@ -1,11 +1,11 @@
 class ScheduleScrapes < ApplicationJob
   queue_as :default
   def perform
-    items = Scrape.where("last_read < ?", 30.minutes.ago)
+    items = Scrape.where("last_read < ?", 10.minutes.ago)
     items.each do |item|
       ScrapeData.new.delay.perform(item.id)
       puts item.id
     end
-    ScheduleScrapes.delay(run_at: 5.minutes.from_now).perform_later
+    ScheduleScrapes.delay(run_at: 1.minutes.from_now).perform_later
   end
 end
